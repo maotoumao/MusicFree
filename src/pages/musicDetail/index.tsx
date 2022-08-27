e990b02cef7c360cd028d578020131eb78e0f16c
猫头猫 <lhx_xjtu@163.com>
2022-08-27 21:41:28 +0800

    feat: image

--- src/pages/musicDetail/index.tsx
+++ src/pages/musicDetail/index.tsx
@@ -4,24 +4,25 @@ import rpx from '@/utils/rpx';
 import {useAtom} from 'jotai';
 import React, {useEffect} from 'react';
 import {Button, StatusBar, StyleSheet, Text, View} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import Background from './components/background';
 import Bottom from './components/bottom';
 import Content from './components/content';
 import NavBar from './components/navBar';
 
 export default function MusicDetail() {
-
   return (
-    <SafeAreaView style={style.wrapper}>
+    <>
       <Background></Background>
-      <View style={style.container}>
-        <StatusBar backgroundColor={'transparent'}></StatusBar>
-        <NavBar></NavBar>
-        <Content></Content>
-        <Bottom></Bottom>
-      </View>
-    </SafeAreaView>
+      <SafeAreaView style={style.wrapper}>
+        <View style={style.container}>
+          <StatusBar backgroundColor={'transparent'}></StatusBar>
+          <NavBar></NavBar>
+          <Content></Content>
+          <Bottom></Bottom>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
@@ -29,7 +30,6 @@ const style = StyleSheet.create({
   wrapper: {
     width: rpx(750),
     flex: 1,
-    backgroundColor: '#333333'
   },
   container: {
     flex: 1,
