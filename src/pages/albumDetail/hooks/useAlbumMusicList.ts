cd2a333a73ea058344759ae3a83fb92d07116ecb
猫头猫 <lhx_xjtu@163.com>
2022-09-10 16:50:11 +0800

    feat: 移除注释

--- src/pages/albumDetail/hooks/useAlbumMusicList.ts
+++ src/pages/albumDetail/hooks/useAlbumMusicList.ts
@@ -8,11 +8,9 @@ export default function useAlbumMusicList(albumItem: IAlbum.IAlbumItem | null) {
     if (albumItem === null) {
       return;
     }
-    console.log('HEREEEEEE');
     PluginManager.getByMedia(albumItem)?.methods?.getAlbumInfo?.(albumItem)?.then(_ => {
-      console.log(_, 'WTF');
       setMusicList(_?.musicList ?? []);
-    })?.catch(console.log);
+    })?.catch();
   }, []);
   return musicList;
 }
