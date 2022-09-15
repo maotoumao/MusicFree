6c578a4d62078e3a032da89b1a01ca201ca03312
猫头猫 <lhx_xjtu@163.com>
2022-09-15 22:28:18 +0800

    feat: logo

--- src/pages/setting/settingTypes/backupSetting.tsx
+++ src/pages/setting/settingTypes/backupSetting.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import rpx from '@/utils/rpx';
+import ThemeText from '@/components/base/themeText';
 
 export default function BackupSetting() {
-    return <View style={style.wrapper} />;
+    return (
+        <View style={style.wrapper}>
+            <ThemeText>还没做完...再等等吧~</ThemeText>
+        </View>
+    );
 }
 
 const style = StyleSheet.create({
