78fb5cb513e484f31720ce27943779f152c23c5d
猫头猫 <lhx_xjtu@163.com>
2023-01-22 20:57:44 +0800

    fix: 歌单详情页背景图

--- src/pages/musicDetail/components/background.tsx
+++ src/pages/musicDetail/components/background.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, StyleSheet} from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
 import MusicQueue from '@/core/musicQueue';
 
 export default function Background() {
@@ -7,19 +7,32 @@ export default function Background() {
     return (
         <>
             {musicItem?.artwork && (
-                <Image
-                    style={style.blur}
-                    blurRadius={18}
-                    source={{
-                        uri: musicItem.artwork,
-                    }}
-                />
+                <>
+                    <View style={style.background} />
+                    <Image
+                        style={style.blur}
+                        blurRadius={18}
+                        source={{
+                            uri: musicItem.artwork,
+                        }}
+                    />
+                </>
             )}
         </>
     );
 }
 
 const style = StyleSheet.create({
+    background: {
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: '#000',
+    },
     blur: {
         width: '100%',
         height: '100%',
