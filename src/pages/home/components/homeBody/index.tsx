1d8853ac5d2700e07a21c4ffb7a6f6954ebba8e2
猫头猫 <lhx_xjtu@163.com>
2024-01-01 22:24:22 +0800

    fix: 无法滚动

--- src/pages/home/components/homeBody/index.tsx
+++ src/pages/home/components/homeBody/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import globalStyle from '@/constants/globalStyle';
-import {ScrollView} from 'react-native-gesture-handler';
 import Operations from './operations';
 import Sheets from './sheets';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export default function HomeBody() {
     return (
         <ScrollView
             style={globalStyle.fwflex1}
-            contentContainerStyle={globalStyle.fwflex1}
             showsVerticalScrollIndicator={false}>
             <Operations />
             <Sheets />
