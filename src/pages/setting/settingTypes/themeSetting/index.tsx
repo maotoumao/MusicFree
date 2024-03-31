b3fc85f367ba7edcc277793f13a2ecb206bb5a75
猫头猫 <lhx_xjtu@163.com>
2024-03-31 23:31:51 +0800

    feat: publish 0.3.0

--- src/pages/setting/settingTypes/themeSetting/index.tsx
+++ src/pages/setting/settingTypes/themeSetting/index.tsx
@@ -4,14 +4,14 @@ import rpx from '@/utils/rpx';
 import Mode from './mode';
 import Background from './background';
 import {ScrollView} from 'react-native-gesture-handler';
-import AppLogoSetting from './appLogoSetting';
+// import AppLogoSetting from './appLogoSetting';
 
 export default function ThemeSetting() {
     return (
         <ScrollView style={style.wrapper}>
             <Mode />
             <Background />
-            <AppLogoSetting />
+            {/* <AppLogoSetting /> */}
         </ScrollView>
     );
 }
