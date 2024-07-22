5574f3153953d79ed985c1316272d24227636a39
猫头猫 <lhx_xjtu@163.com>
2024-07-22 20:41:23 +0800

    refactor: 移除多余代码

--- src/pages/setting/settingTypes/themeSetting/index.tsx
+++ src/pages/setting/settingTypes/themeSetting/index.tsx
@@ -4,14 +4,12 @@ import rpx from '@/utils/rpx';
 import Mode from './mode';
 import Background from './background';
 import {ScrollView} from 'react-native-gesture-handler';
-// import AppLogoSetting from './appLogoSetting';
 
 export default function ThemeSetting() {
     return (
         <ScrollView style={style.wrapper}>
             <Mode />
             <Background />
-            {/* <AppLogoSetting /> */}
         </ScrollView>
     );
 }
