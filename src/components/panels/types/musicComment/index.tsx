717fb284d304793622556c983bbf8b4ba7af19d8
maotoumao <lhx_xjtu@163.com>
2025-01-29 18:15:52 +0800

    fix: 编辑歌单信息样式异常

--- src/components/panels/types/musicComment/index.tsx
+++ src/components/panels/types/musicComment/index.tsx
@@ -1,88 +1,25 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import rpx from '@/utils/rpx';
-import PanelFullscreen from '@/components/panels/base/panelFullscreen.tsx';
-import AppBar from '@/components/base/appBar.tsx';
-import {hidePanel} from '@/components/panels/usePanel.ts';
-import globalStyle from '@/constants/globalStyle.ts';
-import VerticalSafeAreaView from '@/components/base/verticalSafeAreaView.tsx';
-import Loading from '@/components/base/loading.tsx';
-import {FlashList} from '@shopify/flash-list';
-import FastImage from '@/components/base/fastImage';
-import {ImgAsset} from '@/constants/assetsConst.ts';
-import ThemeText from '@/components/base/themeText.tsx';
-import Comment from '@/components/panels/types/musicComment/comment.tsx';
-import useComments from '@/components/panels/types/musicComment/useComments.ts';
-import {RequestStateCode} from '@/constants/commonConst.ts';
-import Empty from '@/components/base/empty.tsx';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import rpx from "@/utils/rpx";
+import PanelFullscreen from "@/components/panels/base/panelFullscreen.tsx";
+import AppBar from "@/components/base/appBar.tsx";
+import { hidePanel } from "@/components/panels/usePanel.ts";
+import globalStyle from "@/constants/globalStyle.ts";
+import VerticalSafeAreaView from "@/components/base/verticalSafeAreaView.tsx";
+import Loading from "@/components/base/loading.tsx";
+import { FlashList } from "@shopify/flash-list";
+import FastImage from "@/components/base/fastImage";
+import { ImgAsset } from "@/constants/assetsConst.ts";
+import ThemeText from "@/components/base/themeText.tsx";
+import Comment from "@/components/panels/types/musicComment/comment.tsx";
+import useComments from "@/components/panels/types/musicComment/useComments.ts";
+import { RequestStateCode } from "@/constants/commonConst.ts";
+import Empty from "@/components/base/empty.tsx";
 
 interface IMusicCommentProps {
     musicItem: IMusic.IMusicItem;
 }
-//
-// const comments: IMedia.IComment[] = [
-//     {
-//         nickName: '猫头猫',
-//         comment: '这是一条测试评论\ndsadasdas',
-//         avatar: 'https://img2.baidu.com/it/u=1028011339,1319212411&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313',
-//         location: '北京',
-//         createAt: Date.now(),
-//         like: 200,
-//     },
-//     {
-//         nickName: '猫头猫',
-//         comment: '这是一条测试评论\ndsadasdas',
-//         avatar: 'https://img2.baidu.com/it/u=1028011339,1319212411&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313',
-//         createAt: Date.now(),
-//         like: 200,
-//     },
-//     {
-//         nickName: '猫头猫',
-//         comment: '这是一条测试评论\ndsadasdas',
-//         avatar: 'https://img2.baidu.com/it/u=1028011339,1319212411&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313',
-//         like: 200000,
-//     },
-//     {
-//         nickName: '猫头猫',
-//         comment: '这是一条测试评论\ndsadasdas',
-//         avatar: 'https://img2.baidu.com/it/u=1028011339,1319212411&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313',
-//     },
-//     {
-//         nickName: '猫头猫',
-//         comment: '这是一条测试评论\ndsadasdas',
-//         avatar: 'https://img2.baidu.com/it/u=1028011339,1319212411&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313',
-//         createAt: Date.now(),
-//         like: 200,
-//     },
-//     {
-//         nickName: '猫头猫',
-//         comment: '这是一条测试评论\ndsadasdas',
-//         avatar: 'https://img2.baidu.com/it/u=1028011339,1319212411&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313',
-//         createAt: Date.now(),
-//         like: 200,
-//     },
-//     {
-//         nickName: '猫头猫',
-//         comment: '这是一条测试评论\ndsadasdas',
-//         avatar: 'https://img2.baidu.com/it/u=1028011339,1319212411&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313',
-//         createAt: Date.now(),
-//         like: 200,
-//     },
-//     {
-//         nickName: '猫头猫',
-//         comment: '这是一条测试评论\ndsadasdas',
-//         avatar: 'https://img2.baidu.com/it/u=1028011339,1319212411&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313',
-//         createAt: Date.now(),
-//         like: 200,
-//     },
-//     {
-//         nickName: '猫头猫',
-//         comment: '这是一条测试评论\ndsadasdas',
-//         avatar: 'https://img2.baidu.com/it/u=1028011339,1319212411&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313',
-//         createAt: Date.now(),
-//         like: 200,
-//     },
-// ];
+
 
 export default function MusicComment(props: IMusicCommentProps) {
     const {musicItem} = props;
