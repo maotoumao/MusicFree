c57105352be6ee26b76f3f393226132c84720b72
CTZZG <41315541+CTZZG@users.noreply.github.com>
2025-05-20 22:20:06 +0800

    修改UA

--- src/utils/userAgentHelper.ts
+++ src/utils/userAgentHelper.ts
@@ -4,15 +4,5 @@ import DeviceInfo from 'react-native-device-info';
 export function getAppUserAgent(): string {
     const appName = DeviceInfo.getApplicationName();
     const appVersion = DeviceInfo.getVersion();
-
-    if (Platform.OS === 'android') {
-        const osVersion = DeviceInfo.getSystemVersion();
-        const deviceModel = DeviceInfo.getModel();
-        return `Mozilla/5.0 (Linux; Android ${osVersion}; ${deviceModel}; rv:112.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 ${appName}/${appVersion}`;
-    } else if (Platform.OS === 'ios') {
-        const osVersion = DeviceInfo.getSystemVersion().replace(/\./g, '_');
-        const deviceModel = DeviceInfo.getModel();
-        return `Mozilla/5.0 (${deviceModel}; CPU iPhone OS ${osVersion} like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 ${appName}/${appVersion}`;
-    }
-    return `${appName}/${appVersion} (${Platform.OS})`;
+    return `${appName}/${appVersion} (${Platform.OS} ${DeviceInfo.getSystemVersion()}; ${DeviceInfo.getModel()})`;
 }
