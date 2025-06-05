8aa4450503ff7aead4d8479f4c1ba5e87ae91c3e
maotoumao <lhx_xjtu@163.com>
2025-06-05 22:53:07 +0800

    feat: 评论接口支持分页

--- src/components/panels/types/musicComment/index.tsx
+++ src/components/panels/types/musicComment/index.tsx
@@ -6,7 +6,6 @@ import AppBar from "@/components/base/appBar.tsx";
 import { hidePanel } from "@/components/panels/usePanel.ts";
 import globalStyle from "@/constants/globalStyle.ts";
 import VerticalSafeAreaView from "@/components/base/verticalSafeAreaView.tsx";
-import Loading from "@/components/base/loading.tsx";
 import { FlashList } from "@shopify/flash-list";
 import FastImage from "@/components/base/fastImage";
 import { ImgAsset } from "@/constants/assetsConst.ts";
@@ -14,8 +13,9 @@ import ThemeText from "@/components/base/themeText.tsx";
 import Comment from "@/components/panels/types/musicComment/comment.tsx";
 import useComments from "@/components/panels/types/musicComment/useComments.ts";
 import { RequestStateCode } from "@/constants/commonConst.ts";
-import Empty from "@/components/base/empty.tsx";
 import { useI18N } from "@/core/i18n";
+import ListEmpty from "@/components/base/listEmpty";
+import ListFooter from "@/components/base/listFooter";
 
 interface IMusicCommentProps {
     musicItem: IMusic.IMusicItem;
@@ -25,27 +25,25 @@ interface IMusicCommentProps {
 export default function MusicComment(props: IMusicCommentProps) {
     const { musicItem } = props;
 
-    const [reqState, comments] = useComments(musicItem);
+    const [reqState, comments, getMusicComments] = useComments(musicItem);
     const { t } = useI18N();
 
-    let listBody = <></>;
 
-    if (reqState & RequestStateCode.LOADING) {
-        listBody = <Loading />;
-    } else if (reqState === RequestStateCode.ERROR) {
-        listBody = <Empty />;
-    } else {
-        listBody = (
-            <FlashList
-                ListEmptyComponent={<Empty />}
-                estimatedItemSize={100}
-                renderItem={({ item }) => {
-                    return <Comment comment={item} />;
-                }}
-                data={comments}
-            />
-        );
-    }
+    const listBody = <FlashList
+        ListFooterComponent={comments?.length ? <ListFooter state={reqState} onRetry={getMusicComments} /> : null}
+        ListEmptyComponent={<ListEmpty state={reqState} onRetry={getMusicComments} />}
+        estimatedItemSize={100}
+        renderItem={({ item }) => {
+            return <Comment comment={item} />;
+        }}
+        onEndReachedThreshold={0.1}
+        onEndReached={() => {
+            if (reqState === RequestStateCode.IDLE || reqState === RequestStateCode.PARTLY_DONE) {
+                getMusicComments();
+            }
+        }}
+        data={comments}
+    />
 
     return (
         <PanelFullscreen>
