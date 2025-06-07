1284687f2530ec77608823503f4ab3e6ab0f64e3
maotoumao <lhx_xjtu@163.com>
2025-06-07 10:15:33 +0800

    fix: package bug

--- android/app/src/main/java/fun/upup/musicfree/MainApplication.kt
+++ android/app/src/main/java/fun/upup/musicfree/MainApplication.kt
@@ -26,7 +26,8 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())              add(UtilsPackage())
+              // add(MyReactNativePackage())              
+              add(UtilsPackage())
               add(Mp3UtilPackage())
               add(LyricUtilPackage())
               add(MusicWidgetPackage())
